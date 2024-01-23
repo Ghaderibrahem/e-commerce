79fe225f2005afa63e43b707281cc26ccb339848
a.ibrahem.ext <a.ibrahem.ext@tahaluf.ai>
2024-01-23 23:17:15 +0400

    pagination

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -2,22 +2,23 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../../components/SearchBar";
 import { Container } from "react-bootstrap";
-import {getProducts} from '../../rtk/products/thunks'
+import { getProducts } from "../../rtk/products/thunks";
 import CardComponent from "../../components/card";
 import Carousel from "../../components/carousel";
 
 import { useDispatch, useSelector } from "react-redux";
+import Pagination from "../../components/pagination";
 
 function Cart() {
   const searchParams = new URLSearchParams(
     new URL(window?.location?.href).search
   );
   const dispatch = useDispatch();
-  const {products} = useSelector(state=>state.products);
+  const { products } = useSelector((state) => state.products);
 
   useEffect(() => {
-    dispatch(getProducts())
-}, []); 
+    dispatch(getProducts());
+  }, []);
   return (
     <Container
       style={{
@@ -26,7 +27,8 @@ function Cart() {
       }}
     >
       <CardComponent />
-     </Container>
+      <Pagination />
+    </Container>
   );
 }
 
